30c902495db456ff1237248a859e47083fbd744e
Jean-Marc Alliot <jm@alliot.org>
2022-04-26 09:15:22 +0200

    Initial release

---
+++ examples/griewank.rs
@@ -0,0 +1,66 @@
+use ag::{ag,ElemPop,Trng};
+use rand::{Rng};
+
+#[derive(Debug, Clone)]
+struct Data {
+    v: Vec<f64>,
+}
+
+const SIZE: usize = 100;
+const MINV: f64 = -10.0;
+const MAXV: f64 = 10.0;
+
+fn scale(a: f64) -> f64 {
+    if a > MAXV {return MINV + (a - MAXV)}
+    else if a < MINV {return MAXV + (a - MINV)}
+    else {return a}
+}
+
+impl ElemPop for Data {
+    fn new(r: &mut Trng) -> Data {
+	let mut t = Vec::with_capacity(SIZE);
+	for _i in 0..SIZE {t.push(r.gen_range(MINV..MAXV))}
+        return Data {v: t};
+    }
+    fn eval(&self) -> f64 {
+        let t = &self.v;
+	let (mut sum,mut prod) = (0.,1.);
+	for i in 0..SIZE {
+            sum = sum + t[i] * t[i];
+	    prod = prod * t[i].cos()/((i+1) as f64).sqrt();
+	}
+	let res = 10.-(sum/4000.-prod);
+        return res.max(0.);
+    }
+    fn dist(&self, u: &Self) -> f64 {
+	let (t1,t2,mut d) = (&self.v,&u.v,0.);
+	for i in 0..SIZE {d = d+(t1[i]-t2[i])*(t1[i]-t2[i])}
+        return d.sqrt();
+    }
+    fn mutate(&self, r: &mut Trng) -> Data {
+	let mut t = self.v.clone();
+	let i = r.gen_range(0..SIZE);
+        t[i] = scale(t[i] + r.gen_range(-0.5..0.5));
+        return Data {v: t}
+    }
+    fn cross<'a>(e1: &'a mut Data,e2: &'a mut Data,r: &mut Trng) -> (&'a Data, &'a Data) {
+        let a: f64 = r.gen_range(-0.5..1.5);
+	let i = r.gen_range(0..SIZE);
+        let b1 = a * e1.v[i] + (1.0 - a) * e2.v[i];
+        let b2 = a * e2.v[i] + (1.0 - a) * e1.v[i];
+        e1.v[i] = scale(b1);
+        e2.v[i] = scale(b2);
+        return (e1, e2);
+    }
+    fn barycenter(e1: &Self, e2: &Self, n1: u32, n2: u32) -> Self {
+	let mut t = Vec::with_capacity(SIZE);
+        let (fn1,fn2) = (n1 as f64,n2 as f64);
+	for i in 0..SIZE {t.push((fn1 * e1.v[i] + fn2 * e2.v[i]) / (fn1 + fn2))}
+        return Data {v:t};
+    }
+}
+
+fn main() {
+    let (e,v,process_time,wall_clock_time) = ag::<Data>(None);
+    println!("Best: {:?}\nr_fit:{}\nprocess_time:{:?}\nwall_clock_time:{:?}", e,v,process_time,wall_clock_time);
+}
---
+++ src/lib.rs
@@ -0,0 +1,382 @@
+use rand::{Rng,SeedableRng};
+use rayon::prelude::*;
+
+pub type Trng=rand_chacha::ChaCha8Rng;
+
+pub trait ElemPop: Clone + Send {
+    fn new(r: &mut Trng) -> Self;
+    fn eval(&self) -> f64;
+    fn dist(&self, u: &Self) -> f64;
+    fn mutate(&self, r: &mut Trng) -> Self;
+    fn cross<'a>(e1: &'a mut Self,e2: &'a mut Self,r: &mut Trng) -> (&'a Self, &'a Self);
+    fn barycenter(e1: &Self, e2: &Self, n1: u32, n2: u32) -> Self;
+}
+
+#[derive(Debug, Clone)]
+struct Chromosome<T: ElemPop> {
+    data: T,
+    r_fit: Option<f64>,
+    s_fit: f64,
+}
+type Pop<T> = Vec<Chromosome<T>>;
+
+fn normalize_hard<T: ElemPop>(mut p: Pop<T>) -> Pop<T> {
+    let (mini, maxi) = p
+        .iter()
+        .fold((f64::INFINITY, f64::NEG_INFINITY), |(mini, maxi), x| {
+            (mini.min(x.s_fit), maxi.max(x.s_fit))
+        });
+    let diff = maxi - mini;
+    for v in p.iter_mut() {
+        if diff == 0. {v.s_fit = 1.0}
+	else {v.s_fit = (v.s_fit - mini) / diff}
+    }
+    return p;
+}
+
+fn normalize_simple<T: ElemPop>(mut p: Pop<T>) -> Pop<T> {
+    let maxi = p.iter().fold(f64::NEG_INFINITY, |acc, x| acc.max(x.s_fit));
+    for v in p.iter_mut() {
+        if maxi == 0. {v.s_fit = 1.0}
+	else {v.s_fit = v.s_fit / maxi}
+    }
+    return p;
+}
+
+fn sigma_truncation<T: ElemPop>(mut p: Pop<T>) -> Pop<T> {
+    let nb_elems = p.len() as f64;
+    let (sum, ssum) = p.iter().fold((0., 0.), |(a1, a2), x| {(a1 + x.s_fit, a2 + x.s_fit * x.s_fit)});
+    let mean = sum / nb_elems;
+    let sigma = (ssum / nb_elems - mean * mean).sqrt();
+    for x in p.iter_mut() {x.s_fit = (0.0_f64).max(x.s_fit - (mean - 2.0 * sigma))}
+    return p;
+}
+
+fn ranking<T: ElemPop>(mut p: Pop<T>) -> Pop<T> {
+    let nb_elems = p.len();
+    p.sort_by(|a, b| b.s_fit.partial_cmp(&a.s_fit).unwrap());
+    for (i, x) in p.iter_mut().enumerate() {x.s_fit = (nb_elems - i) as f64}
+    return p;
+}
+
+fn scale_fitness<T: ElemPop>(mut p: Pop<T>,scaling: u64,normalize: u64) -> Pop<T> {
+    for x in p.iter_mut() {x.s_fit = x.r_fit.unwrap();}
+    match scaling {
+	0 => {},
+	1 => p = sigma_truncation(p),
+	2 => p = ranking(p),
+	_ => panic!("SCALING invalid")
+    }
+    match normalize {
+	0 => {},
+	1 => p = normalize_simple(p),
+	2 => p = normalize_hard(p),
+	_ => panic!("NORMALIZE invalid")
+    }
+    return p;
+}
+
+fn eval_pop<T: ElemPop>(mut p: Pop<T>,par: bool, evolutive: bool) -> Pop<T> {
+    if par {
+	p.par_iter_mut().for_each(|v| {
+	    if v.r_fit == None || evolutive {v.r_fit = Some(v.data.eval())}
+	})
+    }
+    else {
+	p.iter_mut().for_each(|v| {
+	    if v.r_fit == None || evolutive {v.r_fit = Some(v.data.eval())}
+	})
+    }
+    return p;
+}
+
+fn new_pop<T: ElemPop>(nb_elems: usize,rng: &mut Trng) -> Pop<T> {
+    let mut p: Pop<T> = Vec::with_capacity(nb_elems);
+    for _i in 0..nb_elems {p.push(Chromosome {data: ElemPop::new(rng),r_fit: None,s_fit: 0.,})}
+    return p;
+}
+
+fn find_best<T: ElemPop>(p: &Pop<T>) -> usize {
+    let (mut b,mut ib) = (f64::NEG_INFINITY,None);
+    for (ind, e) in p.iter().enumerate() {
+	let v = e.r_fit.unwrap();
+        if v > b {b = v;ib = Some(ind)}
+    }
+    return ib.unwrap();
+}
+
+// Reproduce with stochastic remainders
+fn reproduce_pop<T: ElemPop>(mut oldp: Pop<T>, bests: &Vec<usize>, rng: &mut Trng) -> Pop<T> {
+    let nb_elems = oldp.len();
+    let mut p: Pop<T> = Vec::with_capacity(nb_elems);
+    let mut rmt: Vec<f64> = Vec::with_capacity(nb_elems);
+
+    let ssum1 = oldp.iter().fold(0., |acc, x| acc + x.s_fit);
+    for e in oldp.iter_mut() {e.s_fit = e.s_fit / ssum1 * (nb_elems as f64)+1e-10}
+    let mut t_rem = nb_elems as i64;
+    for ind in bests.iter() {
+        p.push(oldp[*ind].clone());
+        oldp[*ind].s_fit = (0.0_f64).max(oldp[*ind].s_fit - 1.);
+        t_rem = t_rem - 1;
+    }
+    let ssum2 = oldp.iter().fold(0., |acc, x| acc + x.s_fit);
+    let mut sum = 0;
+    let mut fsum = 0.0;
+    for v in oldp.iter() {
+        let nb = (t_rem as f64) * v.s_fit / ssum2;
+        let inb = nb as i64;
+        let rem = nb - (inb as f64);
+        sum = sum + inb;
+        fsum = fsum + rem;
+        rmt.push(rem);
+        for _i in 0..inb {p.push(v.clone())}
+    }
+    for v in rmt.iter_mut() {*v = *v / fsum}
+    let mut prev = 0.;
+    for v in rmt.iter_mut() {*v = *v + prev;prev = *v}
+    let rem = t_rem - sum;
+    for _i in 0..rem {
+	let f = rng.gen_range(0.0..1.0);
+	let (mut up,mut low)=(nb_elems-1,0);
+	while up!=low {
+	    let med = (up+low)/2;
+	    if f<=rmt[med] {up=med} else {low=med+1}
+	}
+	p.push(oldp[up].clone());
+    }
+    return p;
+}
+
+fn cross_mut<T: ElemPop>(oldp: Pop<T>, nb_prot: usize, pcross: f64, pmut: f64, rng: &mut Trng) -> Pop<T> {
+    let nb_elems = oldp.len();
+    let mut p: Pop<T> = Vec::with_capacity(nb_elems);
+    let nb_cross = ((nb_elems as f64) * pcross / 2.0) as usize;
+    let nb_mut = ((nb_elems as f64) * pmut) as usize;
+    let nbr = (nb_elems as usize) - nb_prot - nb_mut - nb_cross * 2;
+    for i in 0..nb_prot {p.push(oldp[i].clone())}
+    for _i in 0..nb_mut {
+        let ind = rng.gen_range(0..nb_elems);
+        let d = oldp[ind].data.mutate(rng);
+        p.push(Chromosome {data: d,r_fit: None,s_fit: 0.,});
+    }
+    for _i in 0..nb_cross {
+        let ind1 = rng.gen_range(0..nb_elems);
+        let ind2 = (ind1 + rng.gen_range(0..nb_elems - 1) + 1) % nb_elems;
+        let mut a = oldp[ind1].data.clone();
+        let mut b = oldp[ind2].data.clone();
+        let (u, v) = ElemPop::cross(&mut a, &mut b, rng);
+        p.push(Chromosome {data: u.clone(),r_fit: None,s_fit: 0.,});
+        p.push(Chromosome {data: v.clone(),r_fit: None,s_fit: 0.,});
+    }
+    for _i in 0..nbr {
+        let ind = rng.gen_range(0..nb_elems);
+        p.push(oldp[ind].clone());
+    }
+    return p;
+}
+
+#[derive(Debug)]
+struct Cluster<T: ElemPop> {
+    center: T,
+    elems: Vec<usize>,
+    nb_elems: u32,
+    best: usize,
+    v_best: f64,
+}
+
+fn pop_clustering<T: ElemPop>(p: &Pop<T>, dmax: f64) -> Vec<Cluster<T>> {
+    let nb_elems = p.len();
+    let mut clus: Vec<Cluster<T>> = Vec::with_capacity(nb_elems);
+    for (ind, e) in p.iter().enumerate() {
+        let (mut bd,mut bi) = (f64::INFINITY,None);
+        for (ic, c) in clus.iter().enumerate() {
+            let dist = e.data.dist(&c.center);
+            if dist < bd {bd = dist;bi = Some(ic)}
+        }
+        if bd > dmax {
+            let mut c = Cluster {
+                center: e.data.clone(),
+                elems: Vec::new(),
+                nb_elems: 1,
+                best: ind,
+                v_best: e.r_fit.unwrap(),
+            };
+            c.elems.push(ind);
+            clus.push(c);
+        }
+	else {
+            let bi = bi.unwrap();
+            clus[bi].center = ElemPop::barycenter(&clus[bi].center, &e.data, clus[bi].nb_elems, 1);
+            clus[bi].nb_elems = clus[bi].nb_elems + 1;
+            clus[bi].elems.push(ind);
+            if e.r_fit.unwrap() > clus[bi].v_best {
+                clus[bi].v_best = e.r_fit.unwrap();
+                clus[bi].best = ind;
+            }
+        }
+    }
+    return clus;
+}
+
+//octopus purpan genomique
+fn share_fitness<T: ElemPop>(mut p: Pop<T>, clus: &Vec<Cluster<T>>) -> Pop<T> {
+    for c in clus.iter() {
+        for i in c.elems.iter() {p[*i].s_fit = p[*i].s_fit / (c.nb_elems as f64)}
+    }
+    return p;
+}
+
+fn get_bests<T: ElemPop>(mut clus: Vec<Cluster<T>>, mut nbest:Vec<usize>,sfactor: f64, mut max_best: u64) -> Vec<usize> {
+    nbest.clear();
+    clus.sort_by(|a, b| b.v_best.partial_cmp(&a.v_best).unwrap());
+    let vbest = clus[0].v_best;
+    for c in clus.iter() {
+        if c.v_best >= vbest * sfactor {
+            nbest.push(c.best);
+            max_best = max_best - 1;
+            if max_best == 0 {break;}
+        }
+	else {break;}
+    }
+    return nbest;
+}
+
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize,Debug)]
+pub struct Params {
+    nb_elems: usize,
+    nb_gen: u64,
+    pmut: f64,
+    pcross: f64,
+    evolutive: bool,
+    scaling: u64,
+    normalize: u64,
+    elitist: bool,
+    sharing: bool,
+    sfactor: f64,
+    dmax: f64,
+    parallel: bool,
+    verbose:u64,
+    seed: u64,
+    ctrlc: bool
+}
+
+#[derive(Default,Debug)]
+pub struct Timing {
+    eval:Duration,
+    scaling:Duration,
+    clustering:Duration,
+    sshare:Duration,
+    crossmut: Duration,
+    reproduce: Duration,
+    total:Duration
+}
+
+use std::fs;
+use json_comments::StripComments;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::time::{Instant,Duration};
+use cpu_time::ProcessTime;
+pub fn ag<T:ElemPop+std::fmt::Debug>(param:Option<Params>)-> (T,f64,Timing,Timing) {
+    let par:Params;
+
+    match param {
+	None => {
+	    let paths = ["./params.json","~/params.json","examples/params.json"];
+	    let mut i =0;
+	    let path =
+		loop {
+		    if fs::metadata(paths[i]).is_ok() {break paths[i]};
+		    i=i+1;
+		    if i==paths.len() {panic!("No params.json file found in ./ , ~/ or examples/")}
+		};
+	    println!("Parameter file {} found",path);
+	    let contents = fs::read_to_string(path)
+		.expect("Something went wrong reading the file");
+	    let stripped = StripComments::new(contents.as_bytes());
+	    par = serde_json::from_reader(stripped).unwrap();
+	}
+	Some (p) => {par=p}
+    }
+    if par.verbose>=1 {println!("{:?}",par)}
+
+    let shared = Arc::new(AtomicBool::new(false));
+    let shared1 = Arc::clone(&shared);
+    if par.ctrlc {
+	ctrlc::set_handler(move || {
+	    shared1.store(true,Ordering::Relaxed);
+            println!("received Ctrl+C, exiting now");
+	})
+	    .expect("Error setting Ctrl-C handler")
+    }
+
+    if par.parallel {
+	let nb = num_cpus::get_physical();
+	rayon::ThreadPoolBuilder::new().num_threads(nb).build_global().unwrap();
+	if par.verbose>=1 {println!("Using {} cpus",nb)}
+    }
+    
+    let mut rng = Trng::seed_from_u64(par.seed);
+    let mut p: Pop<T> = new_pop(par.nb_elems,&mut rng);
+    if par.verbose>=3 {for val in p.iter() {println!("Init: {:?}", val)}}
+
+    let mut bests=Vec::new();
+    let (mut tpi,mut twi):(Timing,Timing)=(Default::default(),Default::default());
+    let (sptt,swtt) = (ProcessTime::now(),Instant::now());
+    for i in 0..par.nb_gen {
+	bests.clear();
+	let (spt,swt) = (ProcessTime::now(),Instant::now());
+        p = eval_pop(p,par.parallel,par.evolutive);
+	tpi.eval=tpi.eval.saturating_add(spt.elapsed());
+	twi.eval=twi.eval.saturating_add(swt.elapsed());
+	let ib = find_best(&p);
+	if par.verbose>=2 {println!("Gen: {:?} Best: {:?}", i, p[ib])}
+	if par.elitist {bests.push(ib)}
+	
+	let (spt,swt) = (ProcessTime::now(),Instant::now());
+        p = scale_fitness(p,par.scaling,par.normalize);
+	tpi.scaling=tpi.scaling.saturating_add(spt.elapsed());
+	twi.scaling=twi.scaling.saturating_add(swt.elapsed());
+        if par.verbose>=3 {for val in p.iter() {println!("Scale: {:?}", val)}}
+
+	if par.sharing {
+	    let (spt,swt) = (ProcessTime::now(),Instant::now());
+            let clusters = pop_clustering(&p, par.dmax);
+            if par.verbose>=3 {for c in clusters.iter() {println!("{:?}", c)}}
+            p = share_fitness(p, &clusters);
+            if par.verbose>=3 {for val in p.iter() {println!("Share: {:?}", val)}}
+	    tpi.clustering=tpi.clustering.saturating_add(spt.elapsed());
+	    twi.clustering=twi.clustering.saturating_add(swt.elapsed());
+	    if par.verbose>=2 {println!("nb_clus={:?}",clusters.len())}
+	    if par.sfactor<=1.0 {
+		let (spt,swt) = (ProcessTime::now(),Instant::now());
+		let max_best = ((par.nb_elems as f64) * (1.0 - par.pmut - par.pcross)) as u64;
+		bests = get_bests(clusters, bests,par.sfactor, max_best);
+		tpi.sshare=tpi.sshare.saturating_add(spt.elapsed());
+		twi.sshare=twi.sshare.saturating_add(swt.elapsed());
+	    }
+	    if par.verbose>=2 {println!("nb_clus_saved={:?}",bests.len())}
+	}
+        if par.verbose>=3 {for b in bests.iter() {println!("best: {:?}", b)}}
+
+	let (spt,swt) = (ProcessTime::now(),Instant::now());
+        p = reproduce_pop(p, &bests,&mut rng);
+	tpi.reproduce=tpi.reproduce.saturating_add(spt.elapsed());
+	twi.reproduce=twi.reproduce.saturating_add(swt.elapsed());
+        if par.verbose>=3 {for val in p.iter() {println!("Reproduce: {:?}", val)}}
+
+	let (spt,swt) = (ProcessTime::now(),Instant::now());
+	p = cross_mut(p, bests.len(), par.pcross, par.pmut, &mut rng);
+	tpi.crossmut=tpi.crossmut.saturating_add(spt.elapsed());
+	twi.crossmut=twi.crossmut.saturating_add(swt.elapsed());
+        if par.verbose>=3 {for val in p.iter() {println!("CrossMut: {:?}", val)}}
+
+	if shared.load(Ordering::Relaxed) {break}
+    }
+    tpi.total=tpi.total.saturating_add(sptt.elapsed());
+    twi.total=twi.total.saturating_add(swtt.elapsed());
+    p = eval_pop(p,par.parallel,par.evolutive);
+    let ib = find_best(&p);
+    return (p[ib].data.clone(),p[ib].r_fit.unwrap(),tpi,twi)
+}
