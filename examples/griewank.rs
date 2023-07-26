76c55598e95d07c7019579d8ff3fde421a7e3df3
Jean-Marc Alliot <jm@alliot.org>
2023-07-26 15:52:31 +0200

    Further lint

--- examples/griewank.rs
+++ examples/griewank.rs
@@ -6,7 +6,7 @@ const MINV: f64 = -10.0;
 const MAXV: f64 = 10.0;
 
 fn scale(a: f64) -> f64 {
-    if a > MAXV {return MINV + (a - MAXV)}
+    if a > MAXV {MINV + (a - MAXV)}
     else if a < MINV {return MAXV + (a - MINV)}
     else {return a}
 }
@@ -25,28 +25,28 @@ impl ElemPop for EPop {
     fn new(r: &mut Trng) -> EPop {
 	let mut t = Vec::with_capacity(SIZE);
 	for _i in 0..SIZE {t.push(r.gen_range(MINV..MAXV))}
-        return EPop {v: t};
+        EPop {v: t}
     }
     fn eval<U:UserData<EPop>>(&mut self,_u:&U) -> f64 {
         let t = &self.v;
 	let (mut sum,mut prod) = (0.,1.);
-	for i in 0..SIZE {
-            sum = sum + t[i] * t[i];
-	    prod = prod * t[i].cos()/((i+1) as f64).sqrt();
+	for (i,o) in t.iter().enumerate().take(SIZE) {
+            sum += o * o;
+	    prod = prod * o.cos()/((i+1) as f64).sqrt();
 	}
 	let res = 10.-(sum/4000.-prod);
-        return res.max(0.);
+        res.max(0.)
     }
     fn dist(&self, u: &Self) -> f64 {
 	let (t1,t2,mut d) = (&self.v,&u.v,0.);
-	for i in 0..SIZE {d = d+(t1[i]-t2[i])*(t1[i]-t2[i])}
-        return d.sqrt();
+	for i in 0..SIZE {d += (t1[i]-t2[i])*(t1[i]-t2[i])}
+        d.sqrt()
     }
     fn mutate(&self, r: &mut Trng) -> EPop {
 	let mut t = self.v.clone();
 	let i = r.gen_range(0..SIZE);
         t[i] = scale(t[i] + r.gen_range(-0.5..0.5));
-        return EPop {v: t}
+        EPop {v: t}
     }
     fn cross(e1: &mut EPop,e2: &mut EPop,r: &mut Trng) {
         let a: f64 = r.gen_range(-0.5..1.5);
@@ -60,7 +60,7 @@ impl ElemPop for EPop {
 	let mut t = Vec::with_capacity(SIZE);
         let (fn1,fn2) = (n1 as f64,n2 as f64);
 	for i in 0..SIZE {t.push((fn1 * e1.v[i] + fn2 * e2.v[i]) / (fn1 + fn2))}
-        return EPop {v:t};
+        EPop {v:t}
     }
 }
 
